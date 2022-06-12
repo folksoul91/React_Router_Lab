ae67770087e284ddbd0e8b8ab00955dcf96ddd79
Dane Kim <mr.dane@danes-mbp.lan>
2022-06-12 11:14:10 -0400

    finish setting up components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import stockData from "./components/data";
+import Nav from "./components/Nav";
+import Main from "./components/Main";
+import "./App.css";
 
-function App() {
+const App = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav />
+      <Main stockData={stockData} />
     </div>
   );
-}
+};
 
 export default App;
